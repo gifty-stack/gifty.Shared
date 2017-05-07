6db51756dc02b92af28ac316f9656a7e69671b7a
Dariusz Pawlukiewicz <Darek@MacBook-Pro-Dariusz.local>
2017-05-07 19:48:34 +0200

    changed default Nancy Bootstrapper

--- src/Nancy/BootstraperBase.cs
+++ src/Nancy/BootstraperBase.cs
@@ -1,6 +1,7 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using gifty.Shared.IoC;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nancy.Bootstrapper;
 using Nancy.Bootstrappers.Autofac;
@@ -10,10 +11,13 @@ namespace gifty.Shared.Nancy
     public abstract class BootstrapperBase : AutofacNancyBootstrapper
     {
         public static ILifetimeScope BootstraperLifetimeScope;
+
+        protected readonly IConfigurationRoot ConfigurationRoot;
         private readonly IServiceCollection _services;
 
-        public BootstrapperBase(IServiceCollection services)
+        public BootstrapperBase(IServiceCollection services, IConfigurationRoot configurationRoot)
         {
+            ConfigurationRoot = configurationRoot;
             _services = services;
         }
 
